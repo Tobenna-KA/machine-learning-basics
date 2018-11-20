c3e524cb17d771843e8d26945efcc72def147c03
Tobenna-KA <kabanofor@yahoo.com>
2018-11-21 01:20:43 +0300

    Update simple-linear-reg.py

--- simple-linear-reg.py
+++ simple-linear-reg.py
@@ -4,6 +4,8 @@ import pylab as pl
 import numpy as np
 %matplotlib inline
 
+from sklearn import linear_model
+
 # !wget -O FuelConsumption.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/FuelConsumptionCo2.csv
    
 df = pd.read_csv("FuelConsumption.csv")
@@ -18,3 +20,11 @@ msk = np.random.rand(len(df)) < 0.8
 train = df[msk]
 test = df[~msk]
 
+regr = linear_model.LinearRegression()
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit(train_x, train_y)
+
+# The coefficients
+print ('Coefficients: ', regr.coef_)
+print ('Intercept: ',regr.intercept_)
