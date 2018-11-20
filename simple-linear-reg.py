300a3f44411998a47367e07bc96bf16ea697a128
Tobenna-KA <kabanofor@yahoo.com>
2018-11-21 01:20:25 +0300

    Update simple-linear-reg.py

--- simple-linear-reg.py
+++ simple-linear-reg.py
@@ -1,1 +1,20 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+%matplotlib inline
+
+# !wget -O FuelConsumption.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/FuelConsumptionCo2.csv
+   
+df = pd.read_csv("FuelConsumption.csv")
+
+# take a look at the dataset
+plt.scatter(df.ENGINESIZE, df.CO2EMISSIONS,  color='blue')
+plt.xlabel("Engine Size")
+plt.ylabel("Emission")
+plt.show()
+
+msk = np.random.rand(len(df)) < 0.8
+train = df[msk]
+test = df[~msk]
 
