e6bfdf8915056da927fd7357bb534ff02001b663
Tobenna-KA <kabanofor@yahoo.com>
2018-12-09 22:39:09 +0300

    Update and rename multiple-linear-reg.ipynb to multiple-linear-reg.py

---
+++ multiple-linear-reg.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+%matplotlib inline
+
+#download file
+!wget -O FuelConsumption.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/FuelConsumptionCo2.csv
+
+df = pd.read_csv("FuelConsumption.csv")
+
+# take a look at the dataset
+df.head()
